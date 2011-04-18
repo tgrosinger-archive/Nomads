94673eb6d8d1791907275150d4209770a5400f2e
Tony <github2@grosinger.net>
2011-04-18 16:23:29 -0700

    Created World class
    Signed-off-by: Tony <github2@grosinger.net>

--- src/net/grosinger/nomads/Drone.java
+++ src/net/grosinger/nomads/Drone.java
@@ -1,5 +1,6 @@
 package net.grosinger.nomads;
 
+//Interface for users to base their drones off.
 public interface Drone {
 	
 	//Probably needs to be made more specific or something
---
+++ src/net/grosinger/nomads/World.java
@@ -0,0 +1,6 @@
+package net.grosinger.nomads;
+
+//Main class where information about the world is stored
+public class World {
+
+}

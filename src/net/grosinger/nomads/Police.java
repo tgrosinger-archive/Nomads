92387b475988c43c6d8d6f54afc679eb0f34851f
Tony Grosinger <tony@grosinger.net>
2011-08-30 15:39:28 -0700

    -Started deciding on how police will function
    Signed-off-by: Tony Grosinger <tony@grosinger.net>

--- src/net/grosinger/nomads/Police.java
+++ src/net/grosinger/nomads/Police.java
@@ -3,22 +3,25 @@ package net.grosinger.nomads;
 import net.grosinger.nomads.DroneListItem.EnumMove;
 
 /**
- * Enforcer of the law.  Has the ability to arrest drones that are wanted.
- * Police have a move speed of 2 squares/turn and get faster each time they catch a criminal.
- * Captured money and objects are turned into money which funds the police department.
+ * Enforcer of the law. Has the ability to arrest drones that are wanted. Police
+ * have a move speed of 2 squares/turn and get faster each time they catch a
+ * criminal.
+ * 
+ * (Possible Idea) Captured money and objects are turned into money which funds
+ * the police department.
  * 
  * Police have a visible distance of 2X BASE_VISIBLEDISTANCE
  * 
  */
-public class Police implements Drone{
+public class Police implements Drone {
 	// TODO - Implement Police
 
 	private static final boolean DEBUGGINGALL = true;
 
-	//Do not change these
+	// Do not change these
 	private String name;
 	private String UID;
-	
+
 	// Define any variables that you need
 	private DroneTools tools;
 
@@ -33,17 +36,17 @@ public class Police implements Drone{
 	public void setName(String newName) {
 		name = newName;
 	}
-	
+
 	@Override
 	public void setUID(String newUID) {
 		UID = newUID;
 	}
-	
+
 	@Override
 	public String getUID() {
 		return UID;
 	}
-	
+
 	@Override
 	public void setDroneTools(DroneTools newTools) {
 		tools = newTools;
@@ -53,21 +56,35 @@ public class Police implements Drone{
 
 	@Override
 	public EnumMove move() {
+		// A police drone will move semi-randomly, looking for neighbors.
+		// Upon finding neighbors it will scan them to see if any of them are
+		// wanted. If one is wanted it will start to pursue them and attempt to
+		// discern their direction of travel. Based on direction of travel it
+		// will call in reinforcements that are in the area.
+		// TODO - Implement Police Move
 		return null;
 	}
 
 	@Override
 	public Neighbor steal() {
+		// This method is not used by the police drones
 		return null;
 	}
 
 	@Override
 	public Neighbor attack() {
+		// When attacked, a police drone will automatically retaliate, dealing
+		// 2x the damage that was done to itself.
+		// TODO - Implement Police Attack
 		return null;
 	}
 
 	@Override
 	public Upgrade upgrade() {
+		// Police drones do not have upgrades. They will automatically upgrade
+		// throughout the game based on time played.
+		// TODO - Implement Police Upgrade
 		return null;
 	}
+
 }
